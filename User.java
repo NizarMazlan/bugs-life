fe5d0aad70b61b7f17710f9243521048b11b01bc
Mohd.Danish Iskandar <63936376+miamzs2001@users.noreply.github.com>
2021-05-19 16:27:28 +0800

    Add files via upload

--- User.java
+++ User.java
@@ -1,30 +1,56 @@
-package assignmenttest1;
+package assignmenttest2;
 
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.PrintWriter;
+import java.util.ArrayList;
+import java.util.Scanner;
+
+/**
+ *
+ * @author miamz
+ */
 public class User {
-    private String name, email, password;
-    private static int userCount = 0;
-
-    public User(String name, String email, String password) {
-        this.name = name;
+    private String email, username, password;
+    private ArrayList<User> list = new ArrayList<>();
+    
+    public User(String email, String username, String password){
         this.email = email;
+        this.username = username;
         this.password = password;
-        userCount++;
+        
     }
     
-    public String getPassword(){
-        return this.password;
-    }
-    
-    public boolean checkPassword(String password){
-        return password.equals(this.password);
+    public User(){}
+
+    public String getEmail() {
+        return email;
     }
 
-    public String getName() {
-        return name;
+    public String getUsername() {
+        return username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public ArrayList<User> getList() {
+        try{
+            Scanner in = new Scanner(new FileInputStream("User.txt"));
+            while(in.hasNext()){
+                String get = in.nextLine();
+                String info[] = get.split(",");
+                list.add(new User(info[0],info[1],info[2]));
+            }
+            in.close();
+        }catch(FileNotFoundException e){
+            System.out.println("File not found");
+        }
+        if(list.isEmpty())
+            return null;
+        return list;
     }
     
-    
-    public String getEmail(){
-        return this.email;
-    }
 }
